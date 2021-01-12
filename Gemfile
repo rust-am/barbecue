6208094b99ec1c716a04a65725268476043e5811
rustam023 <r.91@mail.ru>
2021-01-12 15:42:49 +0300

    alpha

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ ruby '2.7.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.4'
+
+gem 'devise'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,18 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :events
 
+  before_validation :set_name, on: :create
+
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+  # validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
+  private
+
+  def set_name
+    self.name = "username_#{rand(999)}" if self.name.blank?
+  end
 end
