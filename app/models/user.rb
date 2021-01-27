6d491231fa47a269edcd5d2e1853d38d5780683e
rustam023 <r.91@mail.ru>
2021-01-27 11:29:02 +0300

    fix link_sub bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
     self.name = "username_#{rand(999)}" if self.name.blank?
   end
 
-  def link_subscription
+  def link_subscriptions
     Subscription.where(user_id: nil, user_email: self.email).update_all(user_id: self.id)
   end
 end
