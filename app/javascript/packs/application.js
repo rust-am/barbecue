6fac6d4948df5de5600e61755e8a80bb10089c7a
rustam023 <r.91@mail.ru>
2021-02-04 15:07:04 +0300

    add Ya map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-import 'bootstrap/dist/js/bootstrap'
 
+import 'bootstrap/dist/js/bootstrap'
 import '../styles/application.scss'
+import '../sctrips/map'
 
 const images = require.context('../images', true)
