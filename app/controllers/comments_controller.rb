faa055d0d659d110a5918e4bb18c9bcfea418228
rustam023 <r.91@mail.ru>
2021-02-19 21:21:20 +0300

    missclick fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -42,8 +42,7 @@ class CommentsController < ApplicationController
   end
 
   def notify_subscribers_comment(event, comment)
-    all_emails = (event.subscriptions.map { |subs| subs.user_email } s
-    - comment.user.mail).uniq
+    all_emails = (event.subscriptions.map { |subs| subs.user_email } + [event.user.email] - comment.user.mail).uniq
 
     all_emails.each do |mail|
       EventMailer.comment(event, comment, mail).deliver_now
