d5a065206897de5773c697737274d59f94c23e85
rustam023 <r.91@mail.ru>
2021-02-20 20:46:22 +0300

    modified prod env config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,22 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Setup the mailer config
+  config.action_mailer.default_url_options = { host: 'barbecuee.herokuapp.com'}
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['MAILJET_USERNAME'],
+    :password => ENV['MAILJET_PASSWORD'],
+    :domain => 'heroku.com',
+    :address => 'in-v3.mailjet.com',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
@@ -109,6 +125,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
-  config.action_mailer.delivery_method = :mailjet # do not use in dev mode!
 end
