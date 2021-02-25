7e5232698b143ee05d241de082e142b5b2d5557e
rustam023 <r.91@mail.ru>
2021-02-25 12:17:48 +0300

    add self navigation for credentials

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Rails.application.credentials.mailjet[:sender]
+  default from: Rails.application.credentials.dig(:mailjet, :sender)
 
   layout 'mailer'
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,11 +2,11 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider:              'AWS',
-      region:                Rails.application.credentials.aws[:bucket_region],
-      aws_access_key_id:     Rails.application.credentials.aws[:access_key_id],
-      aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key]
+      region:                Rails.application.credentials.dig(:aws, :bucket_region),
+      aws_access_key_id:     Rails.application.credentials.dig(:aws, :access_key_id),
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key)
     }
 
-    config.fog_directory  = Rails.application.credentials.aws[:bucket_name]
+    config.fog_directory  = Rails.application.credentials.dig(:aws, :bucket_name)
   end
 end
