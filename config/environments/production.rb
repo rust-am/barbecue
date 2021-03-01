d3c919c37af9bb4a97206c7a163491cfc0f5c412
rustam023 <r.91@mail.ru>
2021-03-01 18:05:16 +0300

    changed comfig (working params)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,11 +90,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Setup the mailer config
-  # config.action_mailer.default_url_options = { host: 'http://barbecue.ru.com'}
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'barbecue.ru.com'}
+  config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :mailjet_api
-  
+
   # ActionMailer::Base.smtp_settings = {
   #   :user_name => Rails.application.credentials.dig(:mailjet, :api_key),
   #   :password => Rails.application.credentials.dig(mailjet, :secret_key),
