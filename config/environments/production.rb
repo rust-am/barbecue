4dc02d1b5c9ffb7b2ca31d8858e4e739c86ce945
rustam023 <r.91@mail.ru>
2021-03-01 17:53:57 +0300

    mailjet api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,8 +93,8 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: 'http://barbecue.ru.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
-  # config.action_mailer.delivery_method = :mailjet_api
-  #
+  config.action_mailer.delivery_method = :mailjet_api
+  
   # ActionMailer::Base.smtp_settings = {
   #   :user_name => Rails.application.credentials.dig(:mailjet, :api_key),
   #   :password => Rails.application.credentials.dig(mailjet, :secret_key),
