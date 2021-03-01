6e071ec0ef338f2556fd6ad0e351db5d2a01609b
rustam023 <r.91@mail.ru>
2021-03-01 17:49:30 +0300

    changed locale

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Setup the mailer config
-  config.action_mailer.default_url_options = { host: 'http://barbecue.ru.com'}
+  # config.action_mailer.default_url_options = { host: 'http://barbecue.ru.com'}
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   # config.action_mailer.delivery_method = :mailjet_api
